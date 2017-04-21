4d61d5045d4c96855fb8798d8ed2680997fb2b84
richardxiong <richard_xiong@rescomp-16-322191.stanford.edu>
2017-04-20 22:37:43 -0700

    tagger milestone!!!

--- data_prep/tagger.py
+++ data_prep/tagger.py
@@ -33,7 +33,7 @@ def buildDictionary(schema):
                 # one value could potentially corresponds to several field names
                 query_dict[query_word.lower()] = [] 
             query_dict[query_word.lower()].append(key)
-        if value['value_type'] is 'string':
+        if value['value_type'] == 'string':
             # build string_dict: for field values
             for word in value['value_range']:
                 if word not in string_dict:
@@ -309,6 +309,64 @@ def sentTagging(query, schema):
     return tag_sentence
 
 
+def sentTagging_knn_semantic(query, schema, logic=None):
+    ''' Tag each word in a query with one of the three possible tokens:
+          1. <nan>
+          2. <field: i>
+          3. <value: j>
+          where i, j are the position according to the schema
+      return -- tagged query
+                tagged logical form: where <field: 1> equal <value: 1>, select <field: 2>
+                field correspondence: a list of the corresponding field names in seuquence [Gold, Nation], 
+                                      if <field: 1> is Gold and <field: 2> is Nation
+                value correspondence: a list of the corresponding field values in seuquence ['13', 'China'],
+                                      corresponding to the field at the same position in field_corr
+    '''
+    ### construct schema_vec, a dict with (key = vector for centroids, value = schema field name)###
+    # schema_vec = dict()
+    # for i in range(len(schema)):
+    #     vec_list = field2vecField[schema[i]]
+    #     for j in range(len(vec_list)):
+    #         vec_sign = tuple(vec_list[j]) # vec_list[j]
+    #         #vec_sign = vec_list[j]
+    #         schema_vec[vec_sign] = schema[i]
+    
+    # ### construct schema_vec, a dict with (key = vector for centroids, value = schema field name)###
+    # schema_vec2 = dict()
+    # for i in range(len(schema)):
+    #     vec_list2 = field2vecValue[schema[i]]
+    #     for j in range(len(vec_list2)):
+    #         vec_sign2 = tuple(vec_list2[j]) # vec_list2[j]
+    #         #vec_sign = vec_list[j]
+    #         schema_vec2[vec_sign] = schema[i]
+    
+    # 4th pass find values to closest field name in semantic space (name entities)
+        #          find query words to closest field name in semantic space
+        # if tag[i] is not "<nan>":
+        #     continue
+        # # Nearest neighbor: finding closest clustroid
+        # kneighbors = findKNearestNeighbor(words[i], [np.array(x) for x in (schema_vec.keys() + schema_vec2.keys())], k = len(schema))
+        # if kneighbors is None:
+        #     continue
+        # stat = dict()
+        # for (vector, weight) in kneighbors:
+        #     if tuple(vector) in schema_vec:
+        #         candidate = schema_vec[tuple(vector)]
+        #     else:
+        #         candidate = schema_vec2[tuple(vector)]
+        #     if candidate not in stat:
+        #         stat[candidate] = 0.0
+        #     stat[candidate] += weight
+        # def getvalue(item):
+        #     return item[1]
+        # weightlist = sorted(stat.items(), key=getvalue, reverse=True)
+        # tag[i] = weightlist[0][0]
+        # if the_one is not None:
+        #     tag[i] = schema_vec[the_one]
+    
+    return None
+
+
 def sentTagging_value(query, schema, logic=None):
     ''' Tag each word in a query with one of the three possible tokens:
           1. <nan>
@@ -326,33 +384,11 @@ def sentTagging_value(query, schema, logic=None):
     config = tu.Config()
     field2vecField, field2vecValue = fromWordtoVecList(schema)
     field_dict, value_dict = buildDictionary(schema)
-    #print field_dict
-    #print value_dict
     
     ### prepare query, schema, and initialize tag with <nan> ###
     query = query.lower()
     words = basic_tokenizer(query)
     tag = ["<nan>" for x in words]
-    # schema0 = ['sum', 'diff','less','greater','argmax', 'argmin'] #, 'mean', ]
-    # schema += schema0
-    
-    ### construct schema_vec, a dict with (key = vector for centroids, value = schema field name)###
-    schema_vec = dict()
-    for i in range(len(schema)):
-        vec_list = field2vecField[schema[i]]
-        for j in range(len(vec_list)):
-            vec_sign = tuple(vec_list[j]) # vec_list[j]
-            #vec_sign = vec_list[j]
-            schema_vec[vec_sign] = schema[i]
-    
-    ### construct schema_vec, a dict with (key = vector for centroids, value = schema field name)###
-    schema_vec2 = dict()
-    for i in range(len(schema)):
-        vec_list2 = field2vecValue[schema[i]]
-        for j in range(len(vec_list2)):
-            vec_sign2 = tuple(vec_list2[j]) # vec_list2[j]
-            #vec_sign = vec_list[j]
-            schema_vec2[vec_sign] = schema[i]
     
     filter_words = [',','the','a','an','for','of','in','on','with','than','and',\
     'is','are','do','does','did','has','have','had','what','how','many','number','get']
@@ -395,35 +431,13 @@ def sentTagging_value(query, schema, logic=None):
                 baseline = strSimilarity(words[i], schema[j].lower())
         if tag[i] is not '<nan>':
             tag[i] = '<field>:' + tag[i]
-    
-      # 4th pass find values to closest field name in semantic space (name entities)
-        #          find query words to closest field name in semantic space
-        # if tag[i] is not "<nan>":
-        #     continue
-        # # Nearest neighbor: finding closest clustroid
-        # kneighbors = findKNearestNeighbor(words[i], [np.array(x) for x in (schema_vec.keys() + schema_vec2.keys())], k = len(schema))
-        # if kneighbors is None:
-        #     continue
-        # stat = dict()
-        # for (vector, weight) in kneighbors:
-        #     if tuple(vector) in schema_vec:
-        #         candidate = schema_vec[tuple(vector)]
-        #     else:
-        #         candidate = schema_vec2[tuple(vector)]
-        #     if candidate not in stat:
-        #         stat[candidate] = 0.0
-        #     stat[candidate] += weight
-        # def getvalue(item):
-        #     return item[1]
-        # weightlist = sorted(stat.items(), key=getvalue, reverse=True)
-        # tag[i] = weightlist[0][0]
-        # if the_one is not None:
-        #     tag[i] = schema_vec[the_one]
-    
+      
     tag_sentence = ' '.join(tag)
     tag2 = ["<nan>" for x in tag]
     field_corr = []
     value_corr = []
+    num_field_position = []
+    num_value_position = []
     # count = 0
     for i in range(len(tag2)):
         if tag[i] == "<nan>":
@@ -439,6 +453,10 @@ def sentTagging_value(query, schema, logic=None):
                 value_corr.append("<nan>")
                 idx = len(field_corr) - 1
                 tag2[i] = '<field>:'+str(idx)
+            
+            refers = reference[1].split(';')
+            if config.field2word[refers[0]]['value_type'] != 'string':
+                num_field_position.append((i, idx))
 
         else:
             # check reference[1] == '<num>'
@@ -452,50 +470,100 @@ def sentTagging_value(query, schema, logic=None):
 
             else:
                 if reference[1] == '<num>':
+                    num_value_position.append(i)
                     continue
                 field_corr.append(reference[1])
                 value_corr.append(words[i])
                 idx = len(field_corr) - 1
                 tag2[i] = '<value>:'+str(idx)
     
-    # for i in range(len(tag2)):
-    #     if tag[i] == '<nan>' or tag2[i] != '<nan>':
-    #         continue
-    #     if len(words[i]) == 4:
-    #         # Year like fields
-    #         # TO DO: find corresponding field name (correference model?)
-    #         continue
-    #     l = len(field_corr)
-    #     for j in range(l):
-    #         if config.field2word[field_corr[l-1-j]]['value_type'] == 'int':
-    #             # compare value range
-    #             # go over schema from BACK and check type, only one, then it is; more than one, check which in the field_corr
+    print num_field_position    #[(4, 1), (7, 2)]
+    print num_value_position    #[9]
+    for i in num_value_position:
+        if len(words[i]) == 4:
+            # find Year like fields
+            for j in range(len(schema)):
+                if config.field2word[schema[j]]['value_type'] == 'date':
+                    # find year_like field
+                    the_one = schema[j]
+            if the_one in field_corr:
+                idx = field_corr.index(the_one)
+                tag2[i] = '<value>:'+str(idx)
+                if value_corr[idx] is "<nan>":
+                    value_corr[idx] = words[i]
+                else:
+                    value_corr[idx] += ';'+words[i]
+            else:
+                field_corr.append(the_one)
+                value_corr.append(words[i])
+                idx = len(field_corr) - 1
+                tag2[i] = '<value>:'+str(idx)
+            continue
+        # TO DO: find corresponding field name (correference model = classification model?)
+        # find nearest neighbor for this classification model
+        idx = None
+        nearest_dist = len(tag2)
+        for (j,m) in num_field_position:
+            if np.abs(j - i) < nearest_dist:
+                idx = m
+                nearest_dist = np.abs(j - i)
+        tag2[i] = '<value>:'+str(idx)
+        if value_corr[idx] is "<nan>":
+            value_corr[idx] = words[i]
+        else:
+            value_corr[idx] += ';'+words[i]
                     
     field_corr_sentence = ' '.join(field_corr)
     value_corr_sentence = ' '.join(value_corr)
     tag2_sentence = ' '.join(tag2)
+
+    if logic is not None:
+        tokens = logic.split()
+        newlogic = ['<nan>' for x in tokens]
+        for i in range(len(tokens)):
+            if tokens[i] in field_corr:
+                idx = field_corr.index(tokens[i])
+                newlogic[i] = '<field>:'+str(idx)
+                continue
+            for idx in range(len(value_corr)):
+                if tokens[i].lower() in value_corr[idx].split(';'):
+                    newlogic[i] = '<value>:'+str(idx)
+                    continue
+            if newlogic[i] == '<nan>':
+                newlogic[i] = tokens[i]
+        newlogic_sentence = ' '.join(newlogic)
+    else:
+        newlogic_sentence = None
     # further change the logical forms to new_logical forms
-    return tag_sentence, field_corr_sentence, value_corr_sentence, tag2_sentence
+    return tag_sentence, field_corr_sentence, value_corr_sentence, tag2_sentence, newlogic_sentence
 
 
 f_ta = open('../data/rand_train.ta', 'w')
+f_lox = open('../data/rand_train.lox', 'w')
+f_qux = open('../data/rand_train.qux', 'w')
 with open('../data/rand_train.fi') as f_fi:
     with open('../data/rand_train.qu') as f_qu:
-        sent, query = f_fi.readline(), f_qu.readline()
-        idx = 0
-        while sent and query:
-            idx += 1
-            if idx == 15:
-                break
-            schema = sent.split()
-            tagged, field_corr, value_corr, tagged2 = sentTagging_value(query, schema)
-            print sent
-            print query
-            print tagged
-            print field_corr
-            print value_corr
-            print tagged2
-            print '\n'
-            f_ta.write(tagged2 + '\n')
-            sent, query = f_fi.readline(), f_qu.readline()
+        with open('../data/rand_train.lo') as f_lo:
+            sent, query, logic = f_fi.readline(), f_qu.readline(), f_lo.readline()
+            idx = 0
+            while sent and query and logic:
+                # idx += 1
+                # if idx == 15:
+                #     break
+                schema = sent.split()
+                tagged, field_corr, value_corr, tagged2, newlogical = sentTagging_value(query, schema, logic)
+                print sent
+                print query
+                print logic
+                print tagged
+                print field_corr
+                print value_corr
+                print tagged2
+                print newlogical
+                print '\n'
+                f_lox.write(newlogical + '\n')
+                f_ta.write(tagged2 + '\n')
+                sent, query, logic = f_fi.readline(), f_qu.readline(), f_lo.readline()
 f_ta.close()
+f_lox.close()
+f_qux.close()
