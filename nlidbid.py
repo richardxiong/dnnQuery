41925d7879644cba629d8a174bb7c46a8a6be164
richardxiong <richard_xiong@rescomp-16-322191.stanford.edu>
2017-06-25 18:10:17 -0700

    lower lr

--- nlidbid.py
+++ nlidbid.py
@@ -49,7 +49,7 @@ end of it
 '''
 #==================================================================================
 
-tf.app.flags.DEFINE_float("learning_rate", 0.05 * 0.01, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate", 0.05 * 0.007, "Learning rate.")
 tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.96,
                           "Learning rate decays by this much.")
 tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
