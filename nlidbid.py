1eaccb221a0f98b710cb2c11d8fc7bc2a3755a6f
richardxiong <richard_xiong@Richards-MacBook-Pro-3.local>
2017-06-30 11:12:16 -0700

    recipes

--- nlidbid.py
+++ nlidbid.py
@@ -48,7 +48,7 @@ conventions:
 end of it
 '''
 #==================================================================================
-subset = 'publications'
+subset = 'recipes'
     
 tf.app.flags.DEFINE_float("learning_rate", 0.05 * 0.007, "Learning rate.")
 tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.96,
@@ -100,7 +100,8 @@ FLAGS = tf.app.flags.FLAGS
 #_buckets = [(10, 7), (15, 10), (18, 12)]  # socialnetwork
 #_buckets = [(14, 9), (16, 12), (18, 15)]  # blocks
 #_buckets = [(10, 7), (15, 10), (18, 12)]  # basket
-_buckets = [(10, 7), (14, 9), (17, 15)]  # publications
+#_buckets = [(10, 7), (14, 9), (17, 15)]  # publications
+_buckets = [(10, 7), (14, 10), (16, 13)]  # recipes
 
 def read_data(source_path, target_path, tag_path, max_size=None):
   """Read data from source and target files and put into buckets.
