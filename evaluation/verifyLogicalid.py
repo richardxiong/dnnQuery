9df8e101fe09433c42806dcbc73e3899d2ac20b4
richardxiong <richard_xiong@Richards-MacBook-Pro-3.local>
2017-06-30 11:55:48 -0700

    housing

--- evaluation/verifyLogicalid.py
+++ evaluation/verifyLogicalid.py
@@ -25,7 +25,7 @@ def strSimilarity(word1, word2):
 '''
 input files
 '''
-subset = 'restaurants'
+subset = 'housing'
 
 truth_path = "../Overnight/except_%s" % subset
 output_path = "../evaluation/Overnight/except_%s" % subset
