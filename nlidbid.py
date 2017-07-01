df24b2ec3d7bc56a1a8df4949ef5cd99d07d7328
richardxiong <richard_xiong@Richards-MacBook-Pro-3.local>
2017-06-30 17:16:22 -0700

    X publications

--- nlidbid.py
+++ nlidbid.py
@@ -48,7 +48,7 @@ conventions:
 end of it
 '''
 #==================================================================================
-subset = 'recipes'
+subset = 'publications'
     
 tf.app.flags.DEFINE_float("learning_rate", 0.05 * 0.007, "Learning rate.")
 tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.96,
@@ -107,7 +107,8 @@ FLAGS = tf.app.flags.FLAGS
 #_buckets = [(10, 7), (13, 9), (16, 11)]  # calendar
 #_buckets = [(10, 7), (13, 9), (16, 11), (21, 15)]  # except_calendar
 #_buckets = [(10, 7), (15, 10), (18, 12), (21, 15)]  # except_basket
-_buckets = [(10, 7), (14, 10), (16, 13), (21, 15)]  # except_recipes
+#_buckets = [(10, 7), (14, 10), (16, 13), (21, 15)]  # except_recipes
+_buckets = [(10, 7), (14, 10), (17, 15), (22, 16)]  # except_publications
 
 def read_data(source_path, target_path, tag_path, max_size=None):
   """Read data from source and target files and put into buckets.
@@ -393,7 +394,7 @@ def decode():
               resultLogical.append(resultLogical[-2])
             # Constraint 3, formats
             resultLogical = " ".join(resultLogical)
-            resultLogical = resultLogical.replace('where <field>:1 where', 'where <field>:1 equal <field>:1 where')
+            resultLogical = resultLogical.replace('<field>:1 equal <field>:1', '<field>:1')
             resultLogical = resultLogical.replace('<value>:1 where <field>', '<value>:1 and <field>')
             resultLogical = resultLogical.replace('and where', 'and')
             #resultLogical = " ".join([tf.compat.as_str(rev_fr_vocab[output]) for output in outputs])
