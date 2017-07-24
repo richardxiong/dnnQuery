57fcffe34bba5c57a291b6cfcb71f3023b808340
richardxiong <richard_xiong@Richards-MacBook-Pro-3.local>
2017-07-24 14:24:15 -0700

    X-domain housing

--- evaluation/verifyLogicalid.py
+++ evaluation/verifyLogicalid.py
@@ -25,7 +25,7 @@ def strSimilarity(word1, word2):
 '''
 input files
 '''
-subset = 'calendar'
+subset = 'housing'
 
 truth_path = "../dataover/except_%s" % subset
 output_path = "../evaluation/dataover/except_%s" % subset
