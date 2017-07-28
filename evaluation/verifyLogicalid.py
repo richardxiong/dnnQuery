a3cfea97f1f6cc3b413a9b8432ef4492561fe1bd
richardxiong <richard_xiong@Richards-MacBook-Pro-3.local>
2017-07-28 08:24:45 -0700

    restaurants

--- evaluation/verifyLogicalid.py
+++ evaluation/verifyLogicalid.py
@@ -25,7 +25,7 @@ def strSimilarity(word1, word2):
 '''
 input files
 '''
-subset = 'calendar'
+subset = 'restaurants'
 
 truth_path = "../dataover/except_%s" % subset
 output_path = "../evaluation/dataover/except_%s" % subset
